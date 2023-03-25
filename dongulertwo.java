934d3877b3e0f084f4f447c2d6c2ed2664160bab
ismail <126421146+ismail-aykurt@users.noreply.github.com>
2023-03-25 16:19:49 +0300

    Add files via upload

---
+++ dongulertwo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class dongulertwo {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int number;
+        System.out.print("sayı giriniz:");
+        number= input.nextInt();
+        for(int i=1,j=1; i<number && j<number;i*=4, j*=5) {
+            System.out.println( i + " "+ j);
+        }
+    }
+}
